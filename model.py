c1cfe64271a88f0d05060bf531db7b2db83c97cf
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2025-09-13 11:43:20 +0530

    moe implemented

--- model.py
+++ model.py
@@ -13,6 +13,9 @@ class ModelConfig(BaseModel):
     ffn_dim: int = 512 * 4
     eps: float = 1e-8
     n_blocks: int = 8
+    n_experts: int = 8
+    capacity_factor: float = 1.0
+    alpha_aux_loss: float = 1e-2
 
 def get_freqs_cis(cfg: ModelConfig) -> Tensor:
     head_dim = cfg.embed_dim // cfg.n_heads
@@ -29,10 +32,10 @@ def get_freqs_cis(cfg: ModelConfig) -> Tensor:
 def apply_rot_emb(x: Tensor, freq_cis: Tensor) -> Tensor:
     # x -> bsz, n_heads, seq_len, head_dim
     bsz, n_heads, seq_len, head_dim = x.shape
-    _x = x.clone()
-    _x = _x.view(bsz, n_heads, seq_len, head_dim // 2, 2)
-    _x = torch.view_as_complex(_x) # bsz, n_heads, seq_len, head_dim / 2
-    _x_rot = _x * freq_cis[:, :, :seq_len, :] # bsz, n_heads, seq_len, head_dim / 2
+    _x = x.view(bsz, n_heads, seq_len, head_dim // 2, 2)
+    _x = torch.view_as_complex(_x.to(torch.float32)) # bsz, n_heads, seq_len, head_dim / 2
+    freqs = freq_cis[..., :seq_len, :]
+    _x_rot = _x * freqs.to(_x.dtype) # bsz, n_heads, seq_len, head_dim / 2
     _x_rot = torch.view_as_real(_x_rot) # bsz, n_heads, seq_len, head_dim / 2, 2
     _x_rot = _x_rot.view(bsz, n_heads, seq_len, head_dim)
     return _x_rot
@@ -80,7 +83,7 @@ class MHA(nn.Module):
 
         q, k = apply_rot_emb(q, self.freqs_cis), apply_rot_emb(k, self.freqs_cis)
 
-        wts = (q @ k.T) / math.sqrt(head_dim)
+        wts = (q @ k.transpose(-2, -1)) / math.sqrt(head_dim)
         wts = wts + self.mask[:, :, :seq_len, :seq_len]
         wts = F.softmax(wts, dim = -1)
 
@@ -88,6 +91,16 @@ class MHA(nn.Module):
         y = y.transpose(1, 2).contiguous().view(bsz, seq_len, embed_dim)
         return self.O(y)
 
+
+class Router(nn.Module):
+
+    def __init__(self, cfg: ModelConfig) -> None:
+        super().__init__()
+        self.W = nn.Linear(cfg.embed_dim, cfg.n_experts, bias = False)
+    
+    def forward(self, x: Tensor) -> Tensor:
+        logits = self.W(x.to(torch.float32)) # T, n_experts
+        return F.softmax(logits, dim = -1)
 
 class FFN(nn.Module):
 
@@ -103,13 +116,55 @@ class FFN(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         return self.net(x)
 
+class MoE(nn.Module):
+
+    def __init__(self, cfg: ModelConfig) -> None:
+        super().__init__()
+        self.cfg = cfg
+        self.capacity_factor = cfg.capacity_factor
+        self.n_experts = cfg.n_experts
+        self.router = Router(cfg)
+        self.experts = nn.ModuleList([FFN(cfg) for _ in range(cfg.n_experts)])
+    
+    def forward(self, x: Tensor) -> Tensor:
+        # bsz, seq_len, embed_dim
+        bsz, seq_len, embed_dim = x.shape
+        t = bsz * seq_len
+
+        x_flat = x.view(t, embed_dim) # t, embed_dim
+        expert_probs: Tensor = self.router(x_flat) # t, e
+
+        # calculate aux loss
+        per_token_expert_idxs = torch.argmax(expert_probs, dim=-1) # t
+        per_token_expert_prob = expert_probs.gather(1, per_token_expert_idxs.unsqueeze(-1)).squeeze(1) # t
+        # fraction of tokens routed to an expert
+        f_e = per_token_expert_idxs.bincount(minlength=self.n_experts) / t # e
+        # total probability of tokens routed to an expert
+        p_e = expert_probs.mean(dim=0) # e
+        self.aux_loss = (
+            self.cfg.alpha_aux_loss * self.n_experts * (f_e * p_e).sum()
+        ).to(x.device)
+
+        # calculate max. num of tokens to be routed to an expert
+        cap = max(
+            math.ceil(t * self.capacity_factor / self.n_experts),
+            1
+        )
+        y_out = torch.zeros_like(x_flat)
+        for e in range(self.n_experts):
+            mask = per_token_expert_idxs == e
+            idxs_e = torch.nonzero(mask, as_tuple=False).squeeze(1)[:cap]
+            y_out[idxs_e, :] = per_token_expert_prob[idxs_e] * self.experts[e](x_flat[idxs_e, :])
+        
+        return y_out.view(bsz, seq_len, embed_dim)
+
 
 class Block(nn.Module):
 
     def __init__(self, cfg: ModelConfig) -> None:
         super().__init__()
         self.mha = MHA(cfg)
-        self.ffn = FFN(cfg)
+        self.ffn = MoE(cfg)
 
         self.norm1 = RMSNorm(cfg)
         self.norm2 = RMSNorm(cfg)
@@ -136,3 +191,10 @@ class GPT(nn.Module):
             x = block(x)
 
         return self.lm_head(self.norm(x))
+
+    def moe_aux_loss(self) -> Tensor:
+        _loss = torch.tensor(0.0, device=self.lm_head.weight.device)
+        for block in self.blocks:
+            if hasattr(block.ffn, 'aux_loss'):
+                _loss += block.ffn.aux_loss
+        return _loss    
